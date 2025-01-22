e79ccd7646176b57e19e30421abc88f2fbc07b36
BalacenoiuAlin <alin.balacenoiu02@e-uvt.ro>
2025-01-22 22:30:27 +0200

    Added data to the tables

--- table_schemas.sql
+++ table_schemas.sql
@@ -1,35 +1,37 @@
 SET SQL_SAFE_UPDATES = 0;
-
+    
+DROP DATABASE IF EXISTS instagram;
 CREATE DATABASE instagram;
-USE instagram;
+USE instagram; 
 
-CREATE TABLE users
-(
-	id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE users (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
     created_at TIMESTAMP DEFAULT NOW()
 );
 
 DESC users;
 
+SELECT * FROM users;
+
 DROP TABLE users;
 
-CREATE TABLE photos
-(
-	id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE photos (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     image_url VARCHAR(255) NOT NULL,
-    user_id INT NOT NULL,
+    user_id INTEGER NOT NULL,
     created_at TIMESTAMP DEFAULT NOW(),
     FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
-DROP TABLE photos;
-
 DESC photos;
 
-CREATE TABLE comments 
-(
-	id INTEGER AUTO_INCREMENT PRIMARY KEY,
+SELECT * FROM photos;
+
+DROP TABLE photos;
+
+CREATE TABLE comments (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     comment_text VARCHAR(255) NOT NULL,
     photo_id INTEGER NOT NULL,
     user_id INTEGER NOT NULL,
@@ -38,60 +40,64 @@ CREATE TABLE comments
     FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
-DROP TABLE comments;
-
 DESC comments;
 
-CREATE TABLE likes
-(
-	user_id INT NOT NULL,
-    photo_id INT NOT NULL,
+SELECT * FROM comments;
+
+DROP TABLE comments;
+
+CREATE TABLE likes (
+    user_id INTEGER NOT NULL,
+    photo_id INTEGER NOT NULL,
     created_at TIMESTAMP DEFAULT NOW(),
     FOREIGN KEY(user_id) REFERENCES users(id),
     FOREIGN KEY(photo_id) REFERENCES photos(id),
     PRIMARY KEY(user_id, photo_id)
 );
 
-DROP TABLE likes;
-
 DESC likes;
 
-CREATE TABLE follows
-(
-	follower_id INT,
-    followee_id INT,
+SELECT * FROM likes;
+
+DROP TABLE likes;
+
+CREATE TABLE follows (
+    follower_id INTEGER NOT NULL,
+    followee_id INTEGER NOT NULL,
     created_at TIMESTAMP DEFAULT NOW(),
     FOREIGN KEY(follower_id) REFERENCES users(id),
     FOREIGN KEY(followee_id) REFERENCES users(id),
     PRIMARY KEY(follower_id, followee_id)
 );
 
+DESC follows;
+
+SELECT * FROM follows;
+
 DROP TABLE follows;
 
-DESC likes;
-
-CREATE TABLE tags
-(
-	id INT AUTO_INCREMENT PRIMARY KEY,
-    tag_name VARCHAR(255) UNIQUE,
-    created_at TIMESTAMP DEFAULT NOW()
+CREATE TABLE tags (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  tag_name VARCHAR(255) UNIQUE,
+  created_at TIMESTAMP DEFAULT NOW()
 );
-
-DROP TABLE tags;
 
 DESC tags;
 
-CREATE TABLE photo_tags
-(
-	photo_id INT NOT NULL,
-    tag_id INT NOT NULL,
-    FOREIGN KEY(tag_id) REFERENCES tags(id),
+SELECT * FROM tags;
+
+DROP TABLE tags;
+
+CREATE TABLE photo_tags (
+    photo_id INTEGER NOT NULL,
+    tag_id INTEGER NOT NULL,
     FOREIGN KEY(photo_id) REFERENCES photos(id),
+    FOREIGN KEY(tag_id) REFERENCES tags(id),
     PRIMARY KEY(photo_id, tag_id)
 );
 
-DROP TABLE photo_tags;
-
 DESC photo_tags;
-    
-    
+
+SELECT * FROM photo_tags;
+
+DROP TABLE photo_tags;
